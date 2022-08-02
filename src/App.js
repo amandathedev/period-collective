175e7d7945f6d44f3c7325af0a1c00a8e0816269
Amanda Treutler <amanda@sequincard.com>
2022-08-02 17:37:59 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import './App.css';
-import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages";
+import AboutPage from "./pages/about";
+import ContactPage from "./pages/contact";
+import DonatePage from "./pages/donate";
+import NewsPage from "./pages/news";
+import PartnersPage from "./pages/partners";
+import GetInvolvedPage from "./pages/get-involved";
+import EventsPage from "./pages/events";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<LandingPage />}> */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/donate" element={<DonatePage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/get-involved" element={<GetInvolvedPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
