9eae1adab63a5c1e2a422d08a45f20cf46af70fc
Amanda Treutler <amanda@sequincard.com>
2022-11-18 17:06:04 -0600

    hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
+import Button from "react-bootstrap/Button";
+import "../scss/index.scss";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
-      <h1>landing</h1>
+      <div className="hero-container">
+        <img src="../images/boxes.png" alt="Two people carrying boxes of menstrual products" />
+        
+        <h1 className="hero-text">
+          PERIOD PRODUCTS <br></br>
+          <span className="hero-span">FOR ALL IN </span>CHICAGO.
+        </h1>
+        <Button className="blue-button">Get Involved</Button>
+      </div>
     </div>
   );
 };
