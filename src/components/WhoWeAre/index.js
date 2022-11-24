048171646bdf754d627fda3996047e94f6af926a
Amanda Treutler <amanda@sequincard.com>
2022-11-24 10:02:03 -0600

    images

--- src/components/WhoWeAre/index.js
+++ src/components/WhoWeAre/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
+import './index.scss';
 
 const WhoWeAre = () => {
   return (
-    <div className='who-we-are site-container'>
+    <div className="who-we-are site-container">
       <h2>Who we are</h2>
-      <p>
-        The Period Collective is a 501(3)c Nonprofit in Chicago that provides
-        period products to local charity centers to help provide dignity to all
-        Chicagoans.
-      </p>
+      <div className="">
+        <Image src="./images/logo-horiz.png" />
+        <p>
+          The Period Collective is a 501(3)c Nonprofit in Chicago that provides
+          period products to local charity centers to help provide dignity to
+          all Chicagoans.
+        </p>
+      </div>
       <Button className="blue-button">Learn more</Button>
     </div>
   );
