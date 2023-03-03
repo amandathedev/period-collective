012e6bb04c41f7a75bb9a17baece09d9ba1b4862
Amanda Treutler <amanda@sequincard.com>
2023-03-03 14:32:28 -0600

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const ContactPage = () => {
       <div className="site-wrap">
         <Header />
         <PageNameHeader pageName="Contact Us" />
-        <div className="donate-supplies-callout">
+        <div className="donate-supplies-callout mt-4">
           <div className="left">
             <h2>Our Mailing Address</h2>
             <address>
@@ -30,6 +30,13 @@ const ContactPage = () => {
             <img src="./images/supplies.jpg" />
           </div>
         </div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScrsJKF-0nii6v2FZPPYweKSj3C5pKxPGiIPa4595WL6Djkdw/viewform?embedded=true"
+          width="1220"
+          height="729"
+        >
+          Loading…
+        </iframe>
       </div>
       <Footer />
     </>
